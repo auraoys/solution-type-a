a9b70c6995ecde7e73ba103ba571bcba5a5844ba
bit <bit@DESKTOP-6O6H3QB>
2019-02-14 10:59:40 +0900

    Final

--- src/main/java/problem04/Main.java
+++ src/main/java/problem04/Main.java
@@ -1,5 +1,8 @@
 package problem04;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,11 +10,18 @@ public class Main {
 	int answer = 0;
 	int matchcnt = 1;
 	boolean endgame = true;
+	FileWriter writer = null;
+	String Filepath ="result.txt";
 	
 	public static void main(String[] args) {
 
 		Main o = new Main();
+		
+		File file = new File(o.Filepath);
+        
 
+
+		
 		double num = Math.random() * 1000;
 		o.answer = (int) (num % 1000);
 
@@ -20,11 +30,28 @@ public class Main {
 			o.answer = (int) (num % 1000);
 		}
 		
+		System.out.println("숫자를 입력해주세요.");
 		Scanner sc = new Scanner(System.in);
-		
 		while(o.endgame) {
 			o.Judgement(sc.nextInt());
 		}
+		
+	}
+	
+	public void Writer(String input) {
+		  try {
+	            writer = new FileWriter(Filepath, true);
+	            writer.write(input);
+	            writer.flush();
+	        } catch(IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                if(writer != null) writer.close();
+	            } catch(IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
 	}
 
 	public void Judgement(int num) {
@@ -38,9 +65,7 @@ public class Main {
 			System.out.println("입력값이 잘못되었습니다. 3자리 숫자를 입력해주세요");
 			return;
 		}
-
 		for (int i = 0; i < 3; i++) {
-
 			if (answer.substring(i,i+1).equals(input.substring(i,i+1))) {
 				strike++;				
 			} else if (answer.substring(i,i+1).equals(input.substring(1,2))) {
@@ -50,13 +75,11 @@ public class Main {
 			} else {
 				out++;
 			}
-
 		}
 		System.out.println(matchcnt++ +" - S:"+strike+", B:"+ball+", O:"+out);
 		if(strike == 3) {
 			System.out.println("종료");
 			this.endgame = false;
 		}
-		
 	}
 }
